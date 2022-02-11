92ba6e31ad1f6daca1551a17791aca9be6ed9651
salvomel <salv.mel@gmail.com>
2022-02-11 15:34:39 +0100

    controller/model

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Movie;
+
+class HomeController extends Controller
+{
+    public function index() {
+        $movies = Movie::all();
+        dd($movies);
+
+        return view('home');
+    }
+
+    
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,2 @@
+<h1>Movies List</h1>
+
